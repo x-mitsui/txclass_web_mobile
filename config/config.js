42f32fce76fa137fb19943f09c2a6720bb17b693
dzq <x_mitsui@163.com>
2022-04-08 09:18:18 +0800

    chore: 基础api配置

---
+++ config/config.js
@@ -0,0 +1,8 @@
+const URL = {
+  TX_CLASS: '//ke.qq.com/course/',
+  IMG_BASE: '//tximg.codemongo.com/',
+  API_BASE_URL:
+    process.env.NODE_ENV === 'production' ? 'SSSS' : 'http://localhost:3008',
+}
+
+export { URL }
