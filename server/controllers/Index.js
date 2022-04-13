933ea4304b90c438721d7bb0be0d744726beefbb
dzq <x_mitsui@163.com>
2022-04-13 13:35:32 +0800

    refactor: 拆分list页请求

--- server/controllers/Index.js
+++ server/controllers/Index.js
@@ -8,6 +8,7 @@ const { getRecomCourseData } = require('../services/RecomCourse')
 const { getCollectionData } = require('../services/Collection')
 const { getStarTeacherData } = require('../services/Teacher')
 const { getCourseTabData } = require('../services/CourseTab')
+const { getCourseData } = require('../services/Course')
 
 class Index {
   async getHomeData(req, res, next) {
@@ -31,12 +32,22 @@ class Index {
     )
   }
 
-  async getListData(req, res, next) {
+  async getCourseTabData(req, res, next) {
     const courseTabData = await getCourseTabData()
 
     res.end(
       JSON.stringify({
         courseTabData,
+      })
+    )
+  }
+
+  async getCourseData(req, res, next) {
+    const courseData = await getCourseData()
+
+    res.end(
+      JSON.stringify({
+        courseData,
       })
     )
   }
