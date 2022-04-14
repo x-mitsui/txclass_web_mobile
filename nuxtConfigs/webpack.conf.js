35627645f6d1a721390f867958e0fe0706c4f147
dzq <x_mitsui@163.com>
2022-04-14 17:19:46 +0800

    fix: 删除clean-webpack，开发阶段去除上传功能

--- nuxtConfigs/webpack.conf.js
+++ nuxtConfigs/webpack.conf.js
@@ -1,5 +1,4 @@
 const path = require('path')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const { keys, bucket, localPath, zone } = require('../server/configs/qiniu.js')
 const RmNuxtWebpackPlugin = require('./webpackPlugin/RmNuxtConfigPlugin')
 const { QiniuUploadPlugin } = require('./webpackPlugin/QiniuUploadPlugin')
@@ -7,27 +6,26 @@ export const webpackConf = ({ isClient, isDev, isServer }) => {
   const config = { plugins: [] }
   if (isClient) {
     // 只上传客户端资源
-
-    config.plugins.push(
-      new QiniuUploadPlugin({
-        accessKey: keys.ak,
-        secretKey: keys.sk,
-        bucket: bucket.name,
-        publicPath: path.resolve(process.cwd(), localPath),
-        zone,
-        cover: true,
-      })
-    )
+    if (isDev) {
+      config.plugins.push(new RmNuxtWebpackPlugin({}))
+    } else {
+      config.plugins.push(
+        new QiniuUploadPlugin({
+          accessKey: keys.ak,
+          secretKey: keys.sk,
+          bucket: bucket.name,
+          publicPath: path.resolve(process.cwd(), localPath),
+          zone,
+          cover: true,
+        })
+      )
+    }
   }
   if (isDev) {
     config.devtool = 'source-map'
   }
   return {
     ...config,
-    plugins: [
-      new RmNuxtWebpackPlugin(),
-      new CleanWebpackPlugin(),
-      ...config.plugins,
-    ],
+    plugins: [...config.plugins],
   }
 }
